579409c89b7c2134d7f67e009190d57c2c1663bf
Anirudh Konidala <kidskodingclub@gmail.com>
2025-01-05 15:59:31 -0600

    Added INX opcode to CPU

--- src/cpu.rs
+++ src/cpu.rs
@@ -15,16 +15,6 @@ impl CPU {
         }
     }
 
-    fn lda(&mut self, value: u8) {
-        self.register_a = value;
-        self.update_zero_and_negative_flags(self.register_a);
-    }
-
-    fn tax(&mut self) {
-        self.register_x = self.register_a;
-        self.update_zero_and_negative_flags(self.register_x);
-    }
-
     fn update_zero_and_negative_flags(&mut self, result: u8) {
         if self.register_a == 0 {
             self.status |= 0b0000_0010;
@@ -51,9 +41,17 @@ impl CPU {
                     let param = program[self.program_counter as usize];
                     self.program_counter += 1;
 
-                    self.lda(param);
+                    self.register_a = param;
+                    self.update_zero_and_negative_flags(self.register_a);
                 }
-                0xAA => self.tax(),
+                0xAA => {
+                    self.register_x = self.register_a;
+                    self.update_zero_and_negative_flags(self.register_x);
+                }
+                0xE8 => {
+                    self.register_x = self.register_x.wrapping_add(1);
+                    self.update_zero_and_negative_flags(self.register_x);
+                }
                 0x00 => return,
                 _ => todo!()
             }
@@ -62,6 +60,8 @@ impl CPU {
 }
 
 mod test {
+    use crate::cpu::CPU;
+
     mod test_0xa9 {
         use crate::cpu::CPU;
 
@@ -70,22 +70,22 @@ mod test {
             let mut cpu = CPU::new();
             cpu.interpret(vec![0xa9, 0x05, 0x00]);
             assert_eq!(cpu.register_a, 0x05);
-            assert!(cpu.status & 0b0000_0010 == 0b00);
-            assert!(cpu.status & 0b1000_0000 == 0);
+            assert_eq!(cpu.status & 0b0000_0010, 0b00);
+            assert_eq!(cpu.status & 0b1000_0000, 0);
         }
 
         #[test]
         fn test_0xa9_lda_zero_flag() {
             let mut cpu = CPU::new();
             cpu.interpret(vec![0xa9, 0x00, 0x00]);
-            assert!(cpu.status & 0b0000_0010 == 0b10);
+            assert_eq!(cpu.status & 0b0000_0010, 0b10);
         }
 
         #[test]
         fn test_0xa9_lda_negative_flag() {
             let mut cpu = CPU::new();
             cpu.interpret(vec![0xa9, 0x80, 0x00]);
-            assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
+            assert_eq!(cpu.status & 0b1000_0000, 0b1000_0000);
         }
     }
     mod test_0xaa {
@@ -99,5 +99,24 @@ mod test {
 
             assert_eq!(cpu.register_x, 10)
         }
+    }
+    mod test_0xe8 {
+        use crate::cpu::CPU;
+
+        #[test]
+        fn test_inx_overflow() {
+            let mut cpu = CPU::new();
+            cpu.register_x = 0xff;
+            cpu.interpret(vec![0xe8, 0xe8, 0x00]);
+
+            assert_eq!(cpu.register_x, 1)
+        }
+    }
+    #[test]
+    fn test_5_ops_working_together() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+
+        assert_eq!(cpu.register_x, 0xc1)
     }
 }
