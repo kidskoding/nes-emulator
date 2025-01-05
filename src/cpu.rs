a6e248007d5703c37259393667a23281340608df
Anirudh Konidala <kidskodingclub@gmail.com>
2025-01-05 15:41:44 -0600

    Added TAX opcode for NES CPU

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,5 +1,6 @@
 pub struct CPU {
     pub register_a: u8,
+    pub register_x: u8,
     pub status: u8,
     pub program_counter: u8,
 }
@@ -8,8 +9,33 @@ impl CPU {
     pub fn new() -> Self {
         CPU {
             register_a: 0,
+            register_x: 0,
             status: 0,
             program_counter: 0,
+        }
+    }
+
+    fn lda(&mut self, value: u8) {
+        self.register_a = value;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn tax(&mut self) {
+        self.register_x = self.register_a;
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    fn update_zero_and_negative_flags(&mut self, result: u8) {
+        if self.register_a == 0 {
+            self.status |= 0b0000_0010;
+        } else {
+            self.status &= 0b1111_1101;
+        }
+
+        if self.register_a & 0b1000_0000 != 0 {
+            self.status |= 0b1000_0000;
+        } else {
+            self.status &= 0b0111_1111;
         }
     }
 
@@ -24,20 +50,10 @@ impl CPU {
                 0xA9 => {
                     let param = program[self.program_counter as usize];
                     self.program_counter += 1;
-                    self.register_a = param;
 
-                    if self.register_a == 0 {
-                        self.status |= 0b0000_0010;
-                    } else {
-                        self.status &= 0b1111_1101;
-                    }
-
-                    if self.register_a & 0b1000_0000 != 0 {
-                        self.status |= 0b1000_0000;
-                    } else {
-                        self.status &= 0b0111_1111;
-                    }
+                    self.lda(param);
                 }
+                0xAA => self.tax(),
                 0x00 => return,
                 _ => todo!()
             }
@@ -70,6 +86,18 @@ mod test {
             let mut cpu = CPU::new();
             cpu.interpret(vec![0xa9, 0x80, 0x00]);
             assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
+        }
+    }
+    mod test_0xaa {
+        use crate::cpu::CPU;
+
+        #[test]
+        fn test_0xaa_tax_move_a_to_x() {
+            let mut cpu = CPU::new();
+            cpu.register_a = 10;
+            cpu.interpret(vec![0xaa, 0x00]);
+
+            assert_eq!(cpu.register_x, 10)
         }
     }
 }
