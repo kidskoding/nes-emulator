c6fa8741fc06a29adff3e57c3111aa8e1a003dd8
Anirudh Konidala <kidskodingclub@gmail.com>
2025-04-25 12:52:50 -0500

    refactored tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,1 +1,2 @@
 pub mod test_clear_opcodes;
+pub mod test_break_opcodes;
---
+++ tests/test_break_opcodes/mod.rs
@@ -0,0 +1,8 @@
+pub mod test_bcc;
+pub mod test_bcs;
+pub mod test_beq;
+pub mod test_bmi;
+pub mod test_bne;
+pub mod test_bpl;
+pub mod test_bvc;
+pub mod test_bvs;
